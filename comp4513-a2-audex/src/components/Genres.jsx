f5e19ec4ccfc792e3bef65ee5c0a0b942dbf8117
Mitchel Chanthaseng <mchan901@mtroyal.ca>
2026-04-13 19:36:51 -0600

    Catching up to main

--- comp4513-a2-audex/src/components/Genres.jsx
+++ comp4513-a2-audex/src/components/Genres.jsx
@@ -1,4 +1,5 @@
 import SectionHeader from "./SectionHeader.jsx";
+import { Link } from 'react-router-dom';
 
 const Genres = (props) => {
   return (
@@ -8,12 +9,14 @@ const Genres = (props) => {
 
         <div className="flex flex-wrap gap-2">
           {props.genres.map((g, i) => (
+            <Link to="/single-genre/?{g}">
             <button
               key={i}
               className="border border-white/20 px-5 py-2 text-[11px] uppercase tracking-widest hover:bg-cyan-400 hover:text-black transition hover:-translate-y-0.5"
             >
               {g}
             </button>
+            </Link>
           ))}
         </div>
       </div>
